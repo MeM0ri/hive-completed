83599149c780f2f116b2fdc7133a5bfe5b5f00db
Mem0ri <augustwinterart@gmail.com>
2024-11-11 23:54:33 +0200

    fix memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -18,8 +18,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const char	*csrc;
 	size_t		i;
 
-	if (!dest || !src)
-		return (NULL);
 	cdest = (char *)dest;
 	csrc = (const char *)src;
 	i = 0;
