0ea5cd22a84c50575f72ac5f7a19a45245ea9d7f
Mem0ri <augustwinterart@gmail.com>
2024-11-12 22:37:53 +0200

    fix norm in memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -19,7 +19,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t				i;
 
 	if (!dest && !src && n > 0)
-			return (NULL);
+		return (NULL);
 	cdest = (unsigned char *)dest;
 	csrc = (const unsigned char *)src;
 	i = 0;
