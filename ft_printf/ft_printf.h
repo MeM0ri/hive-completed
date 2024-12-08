ea6395316f54d722257908398eccf97c44df1048
Mem0ri <augustwinterart@gmail.com>
2024-12-09 00:57:35 +0200

    add utils

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfokin <alfokin@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/13 12:52:42 by alfokin           #+#    #+#             */
-/*   Updated: 2024/11/13 12:53:26 by alfokin          ###   ########.fr       */
+/*   Created: 2024/12/09 00:47:42 by alfokin           #+#    #+#             */
+/*   Updated: 2024/12/09 00:47:42 by alfokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int	ft_printf(const char *s)
-{
-
-}
+int	ft_printf(const char *s);
+void ft_putchar(char c);
+void ft_putstr(char *s);
+void ft_putnbr(int n);
+void ft_putnbr_base(int n, int base);
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -12,7 +12,37 @@
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *s)
+void ft_putchar(char c)
 {
+	write(1, &c, 1);
+}
 
+void ft_putstr(char *s)
+{
+	while (*s)
+		ft_putchar(*s++);
+}
+
+void ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
+
+void ft_putnbr_base(int n, int base)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= base)
+		ft_putnbr_base(n / base, base);
+	ft_putchar(n % base + '0');
 }
