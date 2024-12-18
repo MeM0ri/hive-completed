c5b286473673779d1947bce5a297dea758eb2ee3
Mem0ri <augustwinterart@gmail.com>
2024-12-18 16:49:45 +0200

    add switch for char tests

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: alfokin <alfokin@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:46:04 by mlitvino          #+#    #+#             */
-/*   Updated: 2024/12/18 15:23:11 by alfokin          ###   ########.fr       */
+/*   Updated: 2024/12/18 16:49:13 by alfokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,80 +14,77 @@
 #include <stdio.h>
 #include <limits.h>
 
-int	main(void)
+int main(void)
 {
-	int len_sys, len_my;
+	int printf_count, ft_printf_count;
+	int switch_toggle = 0;
 
-	printf("_____________________________START_CHAR_TEST____________________________\n\n");
-
-	int ch_loop = 0;
-	int i = -50;
-
-	for (; i < 200 && ch_loop == 1; i++){ // Loop Test
-		printf("sys: ");
-		len_sys = printf("%c", i);
-		printf(" | my: ");
-		fflush(stdout);
-		len_my = ft_printf("%c", i);
-		printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
-		printf("%d\n", i);
-		printf("________________\n");
-	}
-	if (ch_loop == 0) // Normal Test
+	printf("------------------------------------char_test------------------------------------\n\n");
+	switch (switch_toggle)
 	{
-		char a = 'a';
-
-		printf("Normal Test\n");
-		printf("sys: ");
-		len_sys = printf("%c", a);
-		printf(" | my: ");
-		fflush(stdout);
-		len_my = ft_printf("%c", a);
-		printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+		case 0:
+			printf("Loop ASCII Characters\n");
+			for (int i = 32; i < 64 ; i++)
+			{
+				printf("--------------------------------------------\n");
+				printf("ASCII character number to print: %d\n", i);
+				printf("printf: ");
+				printf_count = printf("%c", i);
+				printf(" | ft_printf: ");
+				fflush(stdout);
+				ft_printf_count = ft_printf("%c", i);
+				printf("\nprintf counter: %d | ft_printf counter: %d\n", printf_count, ft_printf_count);
+			}
+			break;
+		default:
+			char a = 'a';
+			printf("Print One Character\n");
+			printf("ASCII character number to print: %d\n", a);
+			printf("printf: ");
+			printf_count = printf("%c", a);
+			printf(" | ft_printf: ");
+			fflush(stdout);
+			ft_printf_count = ft_printf("%c", a);
+			printf("\nprintf counter: %d | ft_printf counter: %d\n", printf_count, ft_printf_count);
+			break;
 	}
+	printf("\n---------------------------------------------------------------------------------\n");
 
-	printf("\n-----------------------------END_CHAR_TEST-----------------------------\n");
-
-
-
-	printf("_____________________________START_STR_TEST____________________________\n\n");
+	printf("------------------------------------str_test------------------------------------\n\n");
 
 	char *pos_str = "HELLO"; // Normal Test
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%s", pos_str);
+	printf_count = printf("%s", pos_str);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%s", pos_str);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%s", pos_str);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	char *null_str = NULL; // Null Test
 
 	printf("NullTest\n");
 	printf("sys: ");
-	len_sys = printf("%s", null_str);
+	printf_count = printf("%s", null_str);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%s", null_str);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%s", null_str);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	char *emp_str = ""; // Empty Test
 
 	printf("Empty Test\n");
 	printf("sys: ");
-	len_sys = printf("%s", emp_str);
+	printf_count = printf("%s", emp_str);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%s", emp_str);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
-
+	ft_printf_count = ft_printf("%s", emp_str);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n-----------------------------END_STR_TEST-----------------------------\n");
-
-
 
 	printf("_____________________________START_PTR_TEST____________________________\n\n");
 
@@ -95,37 +92,35 @@ int	main(void)
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%p", pos_p);
+	printf_count = printf("%p", pos_p);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%p", pos_p);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%p", pos_p);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	void *z_p = (void *)0x0; // NULL Test
 
 	printf("NULL Test\n");
 	printf("sys: ");
-	len_sys = printf("%p", z_p);
+	printf_count = printf("%p", z_p);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%p", z_p);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%p", z_p);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	void *opt_p = (void *)-0x484468464fa; // Optional Test
 
 	printf("Optional Test\n");
 	printf("sys: ");
-	len_sys = printf("%p", opt_p);
+	printf_count = printf("%p", opt_p);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%p", opt_p);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%p", opt_p);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n-----------------------------END_PTR_TEST-----------------------------\n");
-
-
 
 	printf("_____________________________START_INT_TEST____________________________\n\n");
 
@@ -133,88 +128,88 @@ int	main(void)
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", pos_i);
+	printf_count = printf("%i", pos_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", pos_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", pos_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int neg_i = -6554; // Negative Number Test
 
 	printf("Negative Number Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", neg_i);
+	printf_count = printf("%i", neg_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", neg_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", neg_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int zero_i = 0; // Zero Test
 
 	printf("Zero Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", zero_i);
+	printf_count = printf("%i", zero_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", zero_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", zero_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int hex_i = 0xff; // Hex Input Test
 
 	printf("Hex Input Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", hex_i);
+	printf_count = printf("%i", hex_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", hex_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", hex_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int oct_i = 0100; // Octal Input Test
 
 	printf("Octal Input test\n");
 	printf("sys: ");
-	len_sys = printf("%i", oct_i);
+	printf_count = printf("%i", oct_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", oct_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", oct_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int bin_i = 0b100; // Binary Input Test
 
 	printf("Binary Input Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", bin_i);
+	printf_count = printf("%i", bin_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", bin_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", bin_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int min_i = -2147483648; // INT_MIN Test
 
 	printf("INT_MIN Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", min_i);
+	printf_count = printf("%i", min_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", min_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", min_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	int gar_i; // Garbage Test
 
 	printf("Gatbage Test\n");
 	printf("sys: ");
-	len_sys = printf("%i", gar_i);
+	printf_count = printf("%i", gar_i);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%i", gar_i);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%i", gar_i);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n-----------------------------END_INT_TEST-----------------------------\n");
 
@@ -224,36 +219,35 @@ int	main(void)
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%u", pos_u);
+	printf_count = printf("%u", pos_u);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%u", pos_u);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%u", pos_u);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	unsigned int neg_u = -546546554; // Crash Test
 
 	printf("Crash Test\n");
 	printf("sys: ");
-	len_sys = printf("%u", neg_u);
+	printf_count = printf("%u", neg_u);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%u", neg_u);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%u", neg_u);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	unsigned int gar_u; // Garbage Test
 
 	printf("Garbage Test\n");
 	printf("sys: ");
-	len_sys = printf("%u", gar_u);
+	printf_count = printf("%u", gar_u);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%u", gar_u);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%u", gar_u);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n-----------------------------END_UNSIGNED_TEST-----------------------------\n");
-
 
 	printf("_____________________________START_HEX_TEST____________________________\n\n");
 
@@ -261,24 +255,23 @@ int	main(void)
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%x", pos_hex);
+	printf_count = printf("%x", pos_hex);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%x", pos_hex);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%x", pos_hex);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
-
 
 	long long neg_hex = -15456412341; // Negative Number Test
 
 	printf("Negative Number Test\n");
 	printf("sys: ");
-	len_sys = printf("%x", 9223372036854775807LL);
+	printf_count = printf("%x", 9223372036854775807LL);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%x", 9223372036854775807LL);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%x", 9223372036854775807LL);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 
@@ -286,14 +279,13 @@ int	main(void)
 
 	printf("Negative Number Test\n");
 	printf("sys: ");
-	len_sys = printf("%X", up_hex);
+	printf_count = printf("%X", up_hex);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%X", up_hex);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%X", up_hex);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
-
 
 	printf("\n-----------------------------END_HEX_TEST-----------------------------\n");
 
@@ -303,34 +295,33 @@ int	main(void)
 
 	printf("Normal Test\n");
 	printf("sys: ");
-	len_sys = printf("%%", norm_per);
+	printf_count = printf("%%", norm_per);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%%", norm_per);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("%%", norm_per);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	char *neg_per = "Good"; // One Percent Test
 
 	printf("One Percent Test\n");
 	printf("sys: ");
-	len_sys = printf("% ", neg_per);
+	printf_count = printf("% ", neg_per);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("% ", neg_per);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
+	ft_printf_count = ft_printf("% ", neg_per);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n");
 	char *neg_spec = "Bad"; // Wrong Specifier Test
 
 	printf("Wrong Specifier Test\n");
 	printf("sys: ");
-	len_sys = printf("%w", neg_spec);
+	printf_count = printf("%w", neg_spec);
 	printf(" | my: ");
 	fflush(stdout);
-	len_my = ft_printf("%w", neg_spec);
-	printf("\nsys_len: %d | my_len: %d\n", len_sys, len_my);
-
+	ft_printf_count = ft_printf("%w", neg_spec);
+	printf("\nsys_len: %d | my_len: %d\n", printf_count, ft_printf_count);
 
 	printf("\n-----------------------------END_PERC+SPEC_TEST-----------------------------\n");
 
