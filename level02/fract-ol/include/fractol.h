d0c931e90c4ea5ec229d53d21934aa1c5c63f71f
MeM0ri <augustwinterart@gmail.com>
2025-02-25 00:16:10 +0200

    add mlx test

--- level02/fract-ol/include/fractol.h
+++ level02/fract-ol/include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: alfokin <alfokin@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 19:57:28 by alfokin           #+#    #+#             */
-/*   Updated: 2025/02/24 19:57:28 by alfokin          ###   ########.fr       */
+/*   Updated: 2025/02/25 00:00:55 by alfokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <mlx.h>
 # include "ft_printf.h"
 # include "libft.h"
 
-int		main(int argc, char **argv);
+typedef	struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int		main(void);
 
 #endif
--- level02/fract-ol/src/fractol.c
+++ level02/fract-ol/src/fractol.c
@@ -6,31 +6,43 @@
 /*   By: alfokin <alfokin@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 19:57:32 by alfokin           #+#    #+#             */
-/*   Updated: 2025/02/24 19:57:32 by alfokin          ###   ########.fr       */
+/*   Updated: 2025/02/24 23:32:55 by alfokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int		main(int argc, char **argv)
+int	main(void)
 {
-	if (argc != 2)
-	{
-		ft_printf("Usage: %s [fractal]\n", argv[0]);
-		ft_printf("Fractals: mandelbrot, julia, burningship\n");
-		exit(EXIT_FAILURE);
-	}
-	if (ft_strcmp(argv[1], "mandelbrot") == 0)
-		mandelbrot();
-	else if (ft_strcmp(argv[1], "julia") == 0)
-		julia();
-	else if (ft_strcmp(argv[1], "burningship") == 0)
-		burningship();
-	else
-	{
-		ft_printf("Usage: %s [fractal]\n", argv[0]);
-		ft_printf("Fractals: mandelbrot, julia, burningship\n");
-		exit(EXIT_FAILURE);
-	}
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+	// if (argc != 2)
+	// {
+	// 	ft_printf("Usage: %s [fractal]\n", argv[0]);
+	// 	ft_printf("Fractals: mandelbrot, julia, burningship\n");
+	// 	exit(EXIT_FAILURE);
+	// }
+	// if (ft_strcmp(argv[1], "mandelbrot") == 0)
+	// 	mandelbrot();
+	// else if (ft_strcmp(argv[1], "julia") == 0)
+	// 	julia();
+	// else if (ft_strcmp(argv[1], "burningship") == 0)
+	// 	burningship();
+	// else
+	// {
+	// 	ft_printf("Usage: %s [fractal]\n", argv[0]);
+	// 	ft_printf("Fractals: mandelbrot, julia, burningship\n");
+	// 	exit(EXIT_FAILURE);
+	// }
 	exit(EXIT_SUCCESS);
 }
