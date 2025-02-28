b3fabaa4918befdf55b97de3ca6fbab60e4ea7ae
Mem0ri <augustwinterart@gmail.com>
2025-02-28 12:18:16 +0200

    optimization #1

--- level02/fract-ol/src/fractals.c
+++ level02/fract-ol/src/fractals.c
@@ -6,7 +6,7 @@
 /*   By: alfokin <alfokin@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:14:41 by alfokin           #+#    #+#             */
-/*   Updated: 2025/02/27 16:27:15 by alfokin          ###   ########.fr       */
+/*   Updated: 2025/02/28 12:05:45 by alfokin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	calc_mandelbrot(t_fractal *fractal, t_complex_number *c)
 	i = -1;
 	z.real_part = 0;
 	z.imaginary_part = 0;
-	while ((pow(z.real_part, 2) + pow(z.imaginary_part, 2)) < 4 && ++i < fractal->iteration_num)
+	while ((z.real_part * z.real_part + z.imaginary_part * z.imaginary_part) < 4 && ++i < fractal->iteration_num)
 	{
-		re_temp = pow(z.real_part, 2) - pow(z.imaginary_part, 2) + c->real_part;
+		re_temp = z.real_part * z.real_part - z.imaginary_part * z.imaginary_part + c->real_part;
 		z.imaginary_part = 2 * z.real_part * z.imaginary_part + c->imaginary_part;
 		z.real_part = re_temp;
 	}
@@ -39,9 +39,9 @@ int	calc_julia(t_fractal *fractal, t_complex_number *c, int x, int y)
 	i = -1;
 	z.real_part = (x / fractal->zoom) + fractal->offset_x;
 	z.imaginary_part = (y / fractal->zoom) + fractal->offset_y;
-	while ((pow(z.real_part, 2) + pow(z.imaginary_part, 2)) < 4 && ++i < fractal->iteration_num)
+	while ((z.real_part * z.real_part + z.imaginary_part * z.imaginary_part) < 4 && ++i < fractal->iteration_num)
 	{
-		re_temp = pow(z.real_part, 2) - pow(z.imaginary_part, 2) + c->real_part;
+		re_temp = z.real_part * z.real_part - z.imaginary_part * z.imaginary_part + c->real_part;
 		z.imaginary_part = 2 * z.real_part * z.imaginary_part + c->imaginary_part;
 		z.real_part = re_temp;
 	}
@@ -57,9 +57,9 @@ int	calc_burning_ship(t_fractal *fractal, t_complex_number *c)
 	i = -1;
 	z.real_part = 0;
 	z.imaginary_part = 0;
-	while ((pow(z.real_part, 2) + pow(z.imaginary_part, 2)) < 4 && ++i < fractal->iteration_num)
+	while ((z.real_part * z.real_part + z.imaginary_part * z.imaginary_part) < 4 && ++i < fractal->iteration_num)
 	{
-		re_temp = pow(z.real_part, 2) - pow(z.imaginary_part, 2) + c->real_part;
+		re_temp = z.real_part * z.real_part - z.imaginary_part * z.imaginary_part + c->real_part;
 		z.imaginary_part = fabs(2 * z.real_part * z.imaginary_part) + c->imaginary_part;
 		z.real_part = fabs(re_temp);
 	}
